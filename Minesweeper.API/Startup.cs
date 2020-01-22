946c0165f622a835935e251e858b1de4f5a95fe2
Jose VIllaro <jvillaro@gmail.com>
2020-01-22 01:14:02 -0300

    Added swagger

--- Minesweeper.API/Startup.cs
+++ Minesweeper.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Minesweeper.API
 {
@@ -26,6 +27,11 @@ namespace Minesweeper.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minesweeper API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,13 @@ namespace Minesweeper.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minesweeper API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
